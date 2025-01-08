73731774e8e95a768335ad391a22f31fa7000c6f
Dmitri Fedotov <dmitri.fedotov@sap.com>
2025-01-08 15:19:32 +0200

    bump go-makefile-maker and enable image build

---
+++ shell.nix
@@ -0,0 +1,21 @@
+# Copyright 2024 SAP SE
+# SPDX-License-Identifier: Apache-2.0
+
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell {
+  nativeBuildInputs = [
+    ginkgo
+    go-licence-detector
+    go_1_23
+    golangci-lint
+    gotools # goimports
+    kubernetes-controller-tools # controller-gen
+    setup-envtest
+
+    # keep this line if you use bash
+    bashInteractive
+  ];
+}
